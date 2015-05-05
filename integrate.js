99c8b5c7a2be030118c741a27d97f86abbe63a03
Itzblitz98 <gfxscene@gmail.com>
2015-05-05 16:35:45 +0100

    code format + new icon + better album art size

--- integrate.js
+++ integrate.js
@@ -1,148 +1,136 @@
 (function(Nuvola) {
 
-  "use strict";
+    "use strict";
 
-  // Create media player component
-  var player = Nuvola.$object(Nuvola.MediaPlayer);
+    var player = Nuvola.$object(Nuvola.MediaPlayer);
 
-  // Handy aliases
-  var PlaybackState = Nuvola.PlaybackState;
-  var PlayerAction = Nuvola.PlayerAction;
+    var PlaybackState = Nuvola.PlaybackState;
+    var PlayerAction = Nuvola.PlayerAction;
 
-  // Create new WebApp prototype
-  var WebApp = Nuvola.$WebApp();
+    var WebApp = Nuvola.$WebApp();
 
-  // Initialization routines
-  WebApp._onInitWebWorker = function(emitter) {
-    Nuvola.WebApp._onInitWebWorker.call(this, emitter);
+    WebApp._onInitWebWorker = function(emitter) {
+        Nuvola.WebApp._onInitWebWorker.call(this, emitter);
 
-    var state = document.readyState;
-    if (state === "interactive" || state === "complete")
-      this._onPageReady();
-    else
-      document.addEventListener("DOMContentLoaded",
-        this._onPageReady.bind(this));
-  };
+        var state = document.readyState;
+        if (state === "interactive" || state === "complete")
+            this._onPageReady();
+        else
+            document.addEventListener("DOMContentLoaded",
+                this._onPageReady.bind(this));
+    };
 
-  // Page is ready for magic
-  WebApp._onPageReady = function() {
-    // Connect handler for signal ActionActivated
-    Nuvola.actions.connect("ActionActivated", this);
+    WebApp._onPageReady = function() {
+        Nuvola.actions.connect("ActionActivated", this);
 
-    // Set default action states
-    player.setCanPlay(true);
-    player.setCanPause(true);
-    //player.setCanStop(false);
-    player.setCanGoPrev(false);
-    player.setCanGoNext(false);
+        player.setCanPlay(true);
+        player.setCanPause(true);
+        player.setCanGoPrev(false);
+        player.setCanGoNext(false);
 
-    // Configure API hooks
-    this.startApi();
-  };
+        this.startApi();
+    };
 
-  // Loads the KEXP flowplayer API
-  WebApp.startApi = function() {
+    WebApp.startApi = function() {
 
-    this.update();
+        this.update();
 
-  };
+    };
 
 
-  // Extract data from the web page
-  WebApp.update = function() {
-    var status,
-        artist,
-        song,
-        album,
-        album_art,
-        state,
-        track;
+    // Extract data from the web page
+    WebApp.update = function() {
+        var status,
+            artist,
+            song,
+            album,
+            album_art,
+            state,
+            track;
 
-    try{
-      status = document.getElementsByClassName('track-name')[0].innerHTML;
-    } catch (e){
-  		status = "stopped";
-  	}
+        //the player on di takes a second to show up so wait for it if its not there
+        try {
+            status = document.getElementsByClassName('track-name')[0].innerHTML;
+        } catch (e) {
+            status = "stopped";
+        }
 
-    if (status !== "stopped" && status !== "connecting...")	{
+        if (status !== "stopped" && status !== "connecting...") {
 
-      state = PlaybackState.PLAYING;
+            state = PlaybackState.PLAYING;
+            
+            //try has to be here or when song changes this will throw an error.
+            try {
+                artist = document.getElementsByClassName('track-name')[0].getAttribute("title");
+                artist = artist.split("-")[0].trim();
 
-      try{
-        artist = document.getElementsByClassName('track-name')[0].getAttribute("title");
-            artist = artist.split("-")[0].trim();
-            //console.log(artist);
+                song = document.getElementsByClassName('track-name')[0].getAttribute("title");
+                song = song.split("-")[1].trim();
 
-        song = document.getElementsByClassName('track-name')[0].getAttribute("title");
-            song = song.split("-")[1].trim();
-            //console.log(song);
+                album = document.getElementsByClassName('title')[0].innerText;
 
-        album = document.getElementsByClassName('title')[0].innerText;
+                album_art = document.getElementsByClassName('artwork')[1].getElementsByTagName("img")[0].src;
+                album_art = album_art.replace("?size=46x46", "");
 
-        album_art = document.getElementsByClassName('artwork')[1].getElementsByTagName("img")[0].src;
+                track = {
+                    title: song,
+                    artist: artist,
+                    album: album,
+                    artLocation: album_art
+                };
 
-      } catch (e){
-        artist = null;
-        song = null;
-        album = null;
-        album_art = null;
-      }
+            } catch (e) {
 
-        track = {
-          title: song,
-          artist: artist,
-          album: album,
-          artLocation: album_art
-        };
+            }
 
-    } else {
+        } else {
 
-      state = Nuvola.STATE_NONE;
-      track = {
-        title: null,
-        artist: null,
-        album: null,
-        artLocation: null
-      };
-    }
+            state = Nuvola.STATE_NONE;
+            track = {
+                title: null,
+                artist: null,
+                album: null,
+                artLocation: null
+            };
+        }
 
-    player.setPlaybackState(state);
-    player.setTrack(track);
+        player.setPlaybackState(state);
+        player.setTrack(track);
 
 
-    setTimeout(this.update.bind(this), 500);
-  };
+        setTimeout(this.update.bind(this), 500);
+    };
 
-  // Handler of playback actions
-  WebApp._onActionActivated = function(emitter, name, param) {
+    // Handler of playback actions
+    WebApp._onActionActivated = function(emitter, name, param) {
 
-    switch (name) {
-      case PlayerAction.TOGGLE_PLAY:
-        document.getElementsByClassName('controls')[0].getElementsByTagName('a')[0].click();
-        break;
-      case PlayerAction.PLAY:
-        document.getElementsByClassName('controls')[0].getElementsByTagName('a')[0].click();
-        break;
-      case PlayerAction.PAUSE:
-        document.getElementsByClassName('controls')[0].getElementsByTagName('a')[0].click();
-        console.log("pause");
-        break;
-      case PlayerAction.STOP:
-        document.getElementsByClassName('controls')[0].getElementsByTagName('a')[0].click();
-        //not suported
-        break;
-      case PlayerAction.PREV_SONG:
-        //not supported
-        break;
-      case PlayerAction.NEXT_SONG:
-        //not supported
-        break;
-      default:
-        throw {
-          "message": "Not supported."
-        };
-    }
-  };
+        switch (name) {
+            case PlayerAction.TOGGLE_PLAY:
+                document.getElementsByClassName('controls')[0].getElementsByTagName('a')[0].click();
+                break;
+            case PlayerAction.PLAY:
+                document.getElementsByClassName('controls')[0].getElementsByTagName('a')[0].click();
+                break;
+            case PlayerAction.PAUSE:
+                document.getElementsByClassName('controls')[0].getElementsByTagName('a')[0].click();
+                console.log("pause");
+                break;
+            case PlayerAction.STOP:
+                document.getElementsByClassName('controls')[0].getElementsByTagName('a')[0].click();
+                //not suported
+                break;
+            case PlayerAction.PREV_SONG:
+                //not supported
+                break;
+            case PlayerAction.NEXT_SONG:
+                //not supported
+                break;
+            default:
+                throw {
+                    "message": "Not supported."
+                };
+        }
+    };
 
-  WebApp.start();
+    WebApp.start();
 })(this);
